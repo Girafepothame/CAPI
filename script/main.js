7f64b67cb3eb6066504f368f54a9fbdbf0679c18
Girafepothame <loic.yolo@hotmail.fr>
2023-11-27 14:42:55 +0100

    Client side config page

--- script/main.js
+++ script/main.js
@@ -13,20 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
             break;
     }
 
-
-
 })
 
 function homeHandler() {
     let pSelect = _("#nbPlayer");
-    let players = _("#fieldset");
+    let players = _("#fieldset_j");
     pSelect.addEventListener("change", () => {
         players.innerHTML = "";
         let nbPlayer = pSelect.value;
         for (let i = 1; i <= nbPlayer; i++) {
             let label = document.createElement("label");
             label.for = "p" + i;
-            label.innerHTML = "Joueur " + i;
+            label.innerHTML = "Joueur : " + i;
             let player = document.createElement("input");
             player.type = "text";
             player.name = "player" + i;
@@ -43,28 +41,75 @@ function configHandler() {
     let task = _("#tsk");
     let sender = _("#sendTask");
     let tasks = [];
+    let bcklog = _("#bcklog");
 
     task.addEventListener("keydown", (evt) => {
         if (evt.key === 'Enter') {
-            tasks.push(search(task));
-            console.log(tasks);
+            let task_val = search(task)
+            addTask(tasks, task_val);
+            
         }
     })
 
     sender.addEventListener("click", () => {
-        tasks.push(search(task));
-        console.log(tasks);
+        let task_val = search(task)
+        addTask(tasks, task_val);
+        
     })
+
 }
 
 function _(id) {
-    return document.querySelector(id);
+    if (id.substring(0, 1) == '#') {
+        return document.querySelector(id);
+    } else if (id.substring(0, 1) == '.') {
+        return document.querySelectorAll(id);
+    } else {
+        console.log("Mauvais nom d'attribut à sélectionner, veillez à utiliser # ou . pour id ou class");
+    }
 }
 
-function search(el) {
+async function readJSON(event) { // fetch the data of a JSON file into an array
+    const file = event.target.files.item(0)
+    const text = await file.text();
+
+    let tab = JSON.parse(text);
+    let tasks = tab["tasks"];
+
+    for (i in tasks) {
+        console.log(tasks[i]);
+        createTask(tasks[i]);
+    }
+
+  }
+
+function search(el) { // return the value of a text input before reseting the field
     res = el.value;
     el.value = "";
     return res;
+}
+
+function addTask(tab, task) { // Add a task to the task table (in order to create the backlog)
+    task_tab = _("#task_tab");
+    tab.push(task);
+
+    createTask(task);
+}
+
+function createTask(task) { // Create a row inside the task table
+    let row = document.createElement("tr");
+    let t = document.createElement("td");
+    let del = document.createElement("td");
+
+    t.innerHTML = task;
+    del.innerHTML = "<i class='fa fa-trash-o' style='color:red' onclick='removerow(this);'></i>";
+    row.appendChild(t);
+    row.appendChild(del);
+    task_tab.appendChild(row);
+}
+
+function removerow(e) {
+    e.parentNode.parentNode.remove();
 }
 
 function findGetParameter(parameterName) { // Recupérer la page courrante comme en PHP pur archi MVC
--- view/config.php
+++ view/config.php
@@ -1,10 +1,20 @@
+
 <div>
-    <fieldset>
-        <legend>Editer Backlog</legend>
-        <label for="tsk">Tâche :</label>
-        <input type="text" name="task" id="tsk" placeholder="Tâche à ajouter"></input>
-        <button id="sendTask">
-            >
-        </button>
+    <fieldset id="fieldset_t">
+        <div>
+            <legend>Editer Backlog</legend>
+            <label for="tsk">Tâche :</label>
+            <input type="text" name="task" id="tsk" placeholder="Tâche à ajouter"></input>
+            <button id="sendTask"> > </button>
+
+            <label for="bcklog">Fichier de Backlog</label>
+            <input type="file" name="backlog" id="bcklog" accept="application/JSON" onchange="readJSON(event)">
+        </div>
+        <table id="task_tab">
+            <tr>
+                <th>Tâche</th>
+                <th></th>
+            </tr>
+        </table>
     </fieldset>
 </div>
